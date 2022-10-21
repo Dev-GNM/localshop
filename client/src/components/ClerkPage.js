69461c44517e85c78aa45f7291e182562569d6e1
Evans Rutto <robbyevans001@gmail.com>
2022-10-21 17:31:56 +0300

    updated code

--- client/src/components/ClerkPage.js
+++ client/src/components/ClerkPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../App.css"
+// import Navbar from './Navbar/Navbar';
 import {useState,useEffect} from "react"
 import Table from "./ItemsTables/Table"
 import RequestTable from './RequestItemTable.js/RequestTable';
@@ -12,24 +13,6 @@ function ClerkPage() {
   const[isTrue,setIsTrue]=useState(true)
   const[request,setRequest]=useState(null)
 
-
-
-
-  // useEffect(() => {
-  //   fetch(`http://127.0.0.1:3000/items`)
-  //     .then((r) => r.json())
-  //     .then(console.log);
-  // }, []);
-
-  
-  //   useEffect(() => {
-  //   // auto-login
-  //   fetch("http://127.0.0.1:3000/items").then((r) => {
-  //     if (r.ok) {
-  //       r.json().then((item) => console.log(item));
-  //     }
-  //   });
-  // },[]);
 
 
   function handleClick(){
@@ -46,17 +29,14 @@ function ClerkPage() {
           <div className="clerk-banner"><h1 className='clerk-title'>Clerk Page</h1></div>
                 <div class="clerk-container">
           <div class="clerk-navbar">
-            <button className="add-btn" onClick={handleClick}> Add Items</button>
-            <button className="add-btn" onClick={handleRequest}> Request Items</button>
+            <button className="add-btn" onClick={handleClick}> Add Items+</button>
+            <button className="add-btn" onClick={handleRequest}> Request Items+</button>
             <button className="add-btn"> Logout</button>
             </div>
           {isTrue ?<Table/> :null}
-            {request ? <RequestTable/>:null}
-                </div>
-
-                
-
-        </div>
+          {request ? <RequestTable/>:null}
+            </div>
+          </div>
   )
 }
 
